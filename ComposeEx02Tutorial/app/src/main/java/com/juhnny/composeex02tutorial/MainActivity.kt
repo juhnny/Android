ef85092f07dc98b918c899a4b64dcdb4f19c885d
juhnny <iwibest@naver.com>
2022-08-12 23:58:57 +0900

    [ComposeEx02Tutorial] initial commit

---
+++ ComposeEx02Tutorial/app/src/main/java/com/juhnny/composeex02tutorial/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.juhnny.composeex02tutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.juhnny.composeex02tutorial.ui.theme.ComposeEx02TutorialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeEx02TutorialTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeEx02TutorialTheme {
+        Greeting("Android")
+    }
+}
